af590cb3aeca8e0b8d9994b742e333b2ad63bb78
CodecMannProject <nike.style@outlook.com>
2026-01-04 20:27:26 +0200

    Added command-template.py

---
+++ command-template.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+
+# Command metadata
+COMMAND_NAME = "template"
+DESCRIPTION = "General template for new commands"
+CATEGORY = "utilities" # change this to reflect the command category
+ORDER = 2 # change this to reflect new order in help command
+
+class Template(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def template(self, ctx):
+        """Do something useful"""
+
+async def setup(bot):
+    await bot.add_cog(Template(bot))
